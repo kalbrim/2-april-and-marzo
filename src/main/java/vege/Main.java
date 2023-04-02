ba870d2d3e7565aa1339fd722384d16271693330
Brendan Comiskey <comiskeybrendan@gmail.com>
2023-04-02 10:10:53 -0400

    lemonade

---
+++ src/main/java/vege/Main.java
@@ -0,0 +1,34 @@
+package vege;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("largest uprising since");
+    }
+
+//    public int longestPalindrome(String s) {
+//        // values for upper and lowercase letters are
+//        // 97-122 and 65-90
+//        int[] charCounts = new int[128];
+//        // turning string to collection to use enchanced for loop
+//        // then incrment value in charCounts to know number of that letter
+//        // type
+//        for(char c: s.toCharArray()){
+//            //ascii index
+//            charCounts[c]++;
+//        }
+//        // value to return
+//        int result = 0;
+//        // loop through int array charCounts
+//        for(Integer number: charCounts){
+//            // this code is add in even number of letters
+//            result += number / 2 * 2;
+//            // adds odd
+//            if(result%2 == 0 && number%2 == 1){
+//                result += 1;
+//
+//            }
+//        }
+//        return result;
+//
+//    }
+}
